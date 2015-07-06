9df1dfeb2fda88e08b168f2410cd0fbe2ebae01e
juanm95 <juanm95@stanford.edu>
2015-07-05 21:58:40 -0700

    Initial upload. Input with the file name within the directory

---
+++ git-junior.ps1
@@ -0,0 +1,18 @@
+﻿param ( [string]$projectName )
+DO
+{
+$date = Get-Date
+$filePrefix = $date.Month.ToString() + "-" + $date.Day.ToString() + "--"
+$fileSuffixNum = 1
+$folder = $filePrefix + $fileSuffixNum.ToString()
+while(Test-Path .\$projectName\$folder)
+{$fileSuffixNum++;$folder = $filePrefix + $fileSuffixNum.ToString();}
+md .\$projectName\$folder
+$pullFolder = ".\$projectName\$folder\Pull"
+$pushFolder = ".\$projectName\$folder\Push"
+md $pullFolder
+md $pushFolder
+DO {
+    $continueChoice = Read-Host 'Type m or M to add another working folder or X or x to exit'
+} Until ($continueChoice.StartsWith("m") -or $continueChoice.StartsWith("M") -or $continueChoice.StartsWith("x") -or $continueChoice.StartsWith("X"))
+} Until ($continueChoice.StartsWith("x") -or $continueChoice.StartsWith("X"))
